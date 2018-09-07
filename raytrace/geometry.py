054c7f5e585d9d0a6f163c44f932e395f475b9e5
Evelyn Fisher <msevelynfisher@gmail.com>
2018-09-07 23:01:18 +0000

    Masked sphere intersection calculations to avoid unnecessary work.

--- raytrace/geometry.py
+++ raytrace/geometry.py
@@ -1,4 +1,5 @@
 import numpy as np
+from .vector import *
 
 class Sphere:
     
@@ -13,18 +14,27 @@ class Sphere:
         x = positions - self.center
         x_para = directions.scale(x.dot(directions))
         x_perp = x - x_para
-        y_para = directions.scale(-1 * np.sqrt(self.radius ** 2 - x_perp.normsq()))
         
-        distance = (x_para - y_para).norm()
-        miss = np.logical_or(
-            x.normsq() <= self.radius**2,
-            np.logical_or(
-                x_perp.normsq() > 1,
-                x.dot(directions) >= 0
+        mask = np.logical_and(
+            x.normsq() > self.radius**2,
+            np.logical_and(
+                x_perp.normsq() <= self.radius**2,
+                x.dot(directions) < 0
             )
         )
-        np.place(distance, miss, np.inf)
         
-        normal = (x_perp + y_para).unit()
+        x_para_set = x_para.extract(mask)
+        x_perp_set = x_perp.extract(mask)
+        directions_set = directions.extract(mask)
+        
+        y_para_set = directions_set.scale(-1 * np.sqrt(self.radius ** 2 - x_perp_set.normsq()))
+        
+        distance = np.repeat(np.inf, len(mask))
+        distance_set = (x_para_set - y_para_set).norm()
+        np.place(distance, mask, distance_set)
+        
+        normal = V3(0.0, 0.0, 0.0).repeat(len(mask))
+        normal_set = (x_perp_set + y_para_set).unit()
+        normal.place(mask, normal_set)
         
         return (distance, normal)

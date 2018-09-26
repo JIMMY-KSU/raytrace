037069cd41c15b2c8be9d2bbdb570fdf7a8d79be
Evelyn Fisher <msevelynfisher@gmail.com>
2018-09-26 20:55:20 +0000

    Added overall test. Renamed tests for clarity. Removed duplicate tests.

---
+++ tests/test.py
@@ -0,0 +1,7 @@
+import unittest
+from testgeometry import *
+from testrender import *
+from testvector import *
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/testgeometry.py
@@ -0,0 +1,80 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import numpy as np
+from raytrace import *
+import unittest
+
+
+class TestGeometry(unittest.TestCase):
+    
+    def setUp(self):
+        self.geoms = [
+            Sphere(V3(0, 0, 0), 1),
+            Difference(
+                Sphere(V3(0, 0, 0), 1),
+                Sphere(V3(1, 0, 0), 1)
+            ),
+            Intersection(
+                Sphere(V3(0, 0, 0), 1),
+                Sphere(V3(1, 0, 0), 1)
+            )
+        ]
+    
+    def test_intersection(self):
+        for geom in self.geoms:
+            (d, n) = geom.intersections((V3(0, 0, 0), V3(1, 0, 0)))
+            self.assertEqual(type(d), np.ndarray)
+            self.assertEqual(type(n), V3)
+            
+            (d, n) = geom.intersections((V3(0, 0, 0), V3(1, 0, 0)), invert = True)
+            self.assertEqual(type(d), np.ndarray)
+            self.assertEqual(type(n), V3)
+    
+    def test_interior(self):
+        for geom in self.geoms:
+            result = geom.interior(V3(0, 0, 0))
+            self.assertEqual(type(result), np.ndarray)
+    
+    def test_ground(self):
+        ground = Ground(V3(10, 0, 0), V3(1, 1, 0))
+        self.assertEqual(
+            np.array([np.inf]),
+            ground.intersections((
+                V3(-1000, -100, -10),
+                V3(1, 0, 0)
+            ))[0]
+        )
+        self.assertNotEqual(
+            np.array([np.inf]),
+            ground.intersections((
+                V3(1000, 100, 10),
+                V3(-1, 0, 0)
+            ))[0]
+        )
+        self.assertFalse(ground.interior(V3(11, 0, 0)).any())
+        self.assertTrue(ground.interior(V3(0, -100, -10)).all())
+    
+    def test_sphere(self):
+        sphere = Sphere(V3(100, 100, -100), 10)
+        self.assertEqual(
+            np.array([np.inf]),
+            sphere.intersections((
+                V3(0, 0, 0),
+                V3(1, -1, -1).unit()
+            ))[0]
+        )
+        self.assertNotEqual(
+            np.array([np.inf]),
+            sphere.intersections((
+                V3(0, 0, 0),
+                V3(1, 1, -1).unit()
+            ))[0]
+        )
+        self.assertFalse(sphere.interior(V3(0, 0, 0)).any())
+        self.assertFalse(sphere.interior(V3(110, 110, -100)).any())
+        self.assertTrue(sphere.interior(V3(103, 103, -103)).all())
+
+if __name__ == '__main__':
+    unittest.main()
